ae509b2d0e4306225f65805c664483c42f77abf8
yoonjl23 <162141253+yoonjl23@users.noreply.github.com>
2025-05-29 23:25:54 +0900

    main page

---
+++ pc/lib/screens/Login_screens.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class LoginScreens extends StatelessWidget {
+  const LoginScreens({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xFFF2F2F2),
+    );
+  }
+}
