557f8447705cefb144685ebdcd25deb08aa8178a
yoonjl23 <162141253+yoonjl23@users.noreply.github.com>
2025-06-19 17:18:38 +0900

    package

---
+++ pc/lib/screens/Package_screens.dart
@@ -0,0 +1,452 @@
+import 'package:flutter/material.dart';
+import 'package:pc/screens/Activity_detail_screens.dart';
+import 'package:pc/screens/Nav_Screens.dart';
+
+class PackageScreens extends StatefulWidget {
+  final String title;
+  final String userId;
+
+  const PackageScreens({super.key, required this.title, required this.userId});
+
+  @override
+  State<PackageScreens> createState() => _PackageScreensState();
+}
+
+class _PackageScreensState extends State<PackageScreens> {
+  final List<Map<String, String>> programs = [
+    {
+      'title': '[브라운백 시리즈] 오며가며 교양 토크쇼',
+      'imagePath': 'assets/images/targets.png',
+      'point': '10P',
+      'type': '온라인',
+      'duration': '2시간',
+      'field': '자기계발',
+      'category': '강연',
+      'date': '2025년 3월 30일',
+      'location': '수원 컨벤션 센터',
+    },
+    {
+      'title': '(지역사회문제 창의적해결방안 제안)',
+      'imagePath': 'assets/images/cat.jpg',
+      'point': '15P',
+      'type': '오프라인',
+      'duration': '3시간',
+      'field': '사회참여',
+      'category': '공모전',
+      'date': '2025년 3월 30일',
+      'location': '수원 컨벤션 센터',
+    },
+    {
+      'title': '[재맞고] 진로설계 포트폴리오 경진대회 청중평가단 모집',
+      'imagePath': 'assets/images/points.png',
+      'point': '5P',
+      'type': '온라인',
+      'duration': '1시간',
+      'field': '진로탐색',
+      'category': '봉사',
+      'date': '2025년 3월 30일',
+      'location': '수원 컨벤션 센터',
+    },
+  ];
+
+  final List<Map<String, String>> jobs = [
+    {
+      'title': '[토스페이먼츠]',
+      'imagePath': 'assets/images/targets.png',
+      'point': '10P',
+      'type': '온라인',
+      'duration': '2시간',
+      'field': '자기계발',
+      'category': '강연',
+      'date': '2025년 3월 30일',
+      'location': '수원 컨벤션 센터',
+      'theme': '경영/사무',
+      'deadline': '2025년 5월 21일',
+    },
+    {
+      'title': '(지역사회문제 창의적해결방안 제안)',
+      'imagePath': 'assets/images/cat.jpg',
+      'point': '15P',
+      'type': '오프라인',
+      'duration': '3시간',
+      'field': '사회참여',
+      'category': '공모전',
+      'date': '2025년 3월 30일',
+      'location': '수원 컨벤션 센터',
+      'theme': '공모전',
+      'deadline': '2025년 5월 21일',
+    },
+    {
+      'title': '[재맞고] 진로설계 포트폴리오 경진대회 청중평가단 모집',
+      'imagePath': 'assets/images/points.png',
+      'point': '5P',
+      'type': '온라인',
+      'duration': '1시간',
+      'field': '진로탐색',
+      'category': '봉사',
+      'date': '2025년 3월 30일',
+      'location': '수원 컨벤션 센터',
+      'theme': '진로',
+      'deadline': '2025년 5월 21일',
+    },
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    final bool isJobPackage =
+        widget.title.contains('진로') || widget.title.contains('취업');
+
+    final dataList = isJobPackage ? jobs : programs;
+
+    return Scaffold(
+      backgroundColor: const Color(0xFFF2F2F2),
+      appBar: AppBar(
+        backgroundColor: const Color(0xFFF2F2F2),
+        elevation: 0,
+        automaticallyImplyLeading: true,
+        title: const Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              '맞춤형 추천',
+              style: TextStyle(
+                fontFamily: "Roboto",
+                fontWeight: FontWeight.w400,
+                fontSize: 20,
+                color: Color(0xFF7B7B7B),
+              ),
+            ),
+          ],
+        ),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const SizedBox(height: 30),
+            Text(
+              widget.title,
+              style: const TextStyle(
+                fontFamily: "Roboto",
+                fontWeight: FontWeight.w600,
+                fontSize: 24,
+                letterSpacing: -0.72,
+                color: Color(0xFF262626),
+              ),
+            ),
+            const SizedBox(height: 20),
+            Expanded(
+              child: GridView.count(
+                crossAxisCount: 2,
+                crossAxisSpacing: 12,
+                mainAxisSpacing: 12,
+                childAspectRatio: 0.7,
+                children:
+                    dataList.map((program) {
+                      return isJobPackage
+                          ? buildProgramCards(
+                            title: program['title']!,
+                            imagePath: program['imagePath']!,
+                            field: program['field']!,
+                            deadline: getDDay(program['deadline'] ?? ''),
+                            theme: program['theme'] ?? '',
+                            onTap: () {
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                  builder:
+                                      (_) => ActivityDetailScreens(
+                                        userId: widget.userId,
+                                        title: program['title']!,
+                                        imagePath: program['imagePath']!,
+                                        point: program['point']!,
+                                        type: program['type']!,
+                                        duration: program['duration']!,
+                                        field: program['field']!,
+                                        category: program['category']!,
+                                        date: program['date'] ?? '날짜 추후 공지',
+                                        location:
+                                            program['location'] ?? '장소 추후 공지',
+                                      ),
+                                ),
+                              );
+                            },
+                          )
+                          : buildProgramCard(
+                            title: program['title']!,
+                            imagePath: program['imagePath']!,
+                            onTap: () {
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                  builder:
+                                      (_) => ActivityDetailScreens(
+                                        userId: widget.userId,
+                                        title: program['title']!,
+                                        imagePath: program['imagePath']!,
+                                        point: program['point']!,
+                                        type: program['type']!,
+                                        duration: program['duration']!,
+                                        field: program['field']!,
+                                        category: program['category']!,
+                                        date: program['date'] ?? '날짜 추후 공지',
+                                        location:
+                                            program['location'] ?? '장소 추후 공지',
+                                      ),
+                                ),
+                              );
+                            },
+                          );
+                    }).toList(),
+              ),
+            ),
+          ],
+        ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        backgroundColor: const Color(0xFFF2F2F2),
+        currentIndex: 1,
+        onTap: (index) {
+          Navigator.pushReplacement(
+            context,
+            MaterialPageRoute(
+              builder:
+                  (_) => NavScreens(userId: widget.userId, initialIndex: index),
+            ),
+          );
+        },
+        showSelectedLabels: false,
+        showUnselectedLabels: false,
+        selectedItemColor: const Color(0xFF7B7B7B),
+        unselectedItemColor: const Color(0xFF7B7B7B),
+        items: const [
+          BottomNavigationBarItem(icon: Icon(Icons.home_outlined), label: '홈'),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person_outline),
+            label: '마이페이지',
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget buildProgramCard({
+    required String title,
+    required String imagePath,
+    required VoidCallback onTap,
+  }) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        padding: const EdgeInsets.all(12),
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(12),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.grey.withOpacity(0.1),
+              blurRadius: 8,
+              offset: const Offset(0, 2),
+            ),
+          ],
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Row(
+              children: [
+                buildTag("온라인"),
+                const SizedBox(width: 4),
+                buildTag("2주이내"),
+                const SizedBox(width: 4),
+                buildTag("포인트", highlight: true),
+              ],
+            ),
+            const SizedBox(height: 12),
+            ClipRRect(
+              borderRadius: BorderRadius.circular(8),
+              child: Image.asset(
+                imagePath,
+                height: 100,
+                width: double.infinity,
+                fit: BoxFit.cover,
+              ),
+            ),
+            const SizedBox(height: 12),
+            Text(
+              title,
+              style: const TextStyle(
+                fontFamily: "Roboto",
+                fontWeight: FontWeight.w400,
+                fontSize: 12,
+                color: Color(0xFF262626),
+                letterSpacing: -0.36,
+              ),
+            ),
+            const SizedBox(height: 12),
+            Container(
+              padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 6),
+              decoration: BoxDecoration(
+                color: const Color(0xFFFFF1DB),
+                borderRadius: BorderRadius.circular(20),
+              ),
+              child: const Text(
+                '관련 분야',
+                style: TextStyle(
+                  fontSize: 12,
+                  fontWeight: FontWeight.w400,
+                  color: Color(0xFFEA7500),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget buildProgramCards({
+    required String title,
+    required String imagePath,
+    required VoidCallback onTap,
+    required String field,
+    required String deadline,
+    required String theme,
+  }) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        padding: const EdgeInsets.all(12),
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(12),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.grey.withOpacity(0.1),
+              blurRadius: 8,
+              offset: const Offset(0, 2),
+            ),
+          ],
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Stack(
+              children: [
+                ClipRRect(
+                  borderRadius: BorderRadius.circular(8),
+                  child: Image.asset(
+                    imagePath,
+                    height: 100,
+                    width: double.infinity,
+                    fit: BoxFit.cover,
+                  ),
+                ),
+                Positioned(
+                  top: 0,
+                  right: 0,
+                  child: Container(
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: 8,
+                      vertical: 4,
+                    ),
+                    decoration: BoxDecoration(
+                      color: const Color(0xFFDCE9FA),
+                      borderRadius: BorderRadius.circular(12),
+                    ),
+                    child: Text(
+                      deadline,
+                      style: const TextStyle(
+                        fontSize: 12,
+                        fontWeight: FontWeight.w500,
+                        color: Color(0xFF2A6FB0),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            const SizedBox(height: 12),
+            Text(
+              title,
+              style: const TextStyle(
+                fontFamily: "Roboto",
+                fontWeight: FontWeight.w400,
+                fontSize: 12,
+                color: Color(0xFF262626),
+                letterSpacing: -0.03,
+              ),
+            ),
+            const SizedBox(height: 8),
+            Text(
+              '\n$theme',
+              style: const TextStyle(
+                fontFamily: 'Roboto',
+                fontWeight: FontWeight.w400,
+                fontSize: 14,
+                letterSpacing: -0.42,
+                color: Color(0xFF7B7B7B),
+              ),
+            ),
+            Container(
+              padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 6),
+              decoration: BoxDecoration(
+                color: const Color(0xFFFFF1DB),
+                borderRadius: BorderRadius.circular(20),
+              ),
+              child: Text(
+                field,
+                style: const TextStyle(
+                  fontSize: 12,
+                  fontWeight: FontWeight.w400,
+                  color: Color(0xFFEA7500),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget buildTag(String text, {bool highlight = false}) {
+    return Container(
+      padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 4),
+      decoration: BoxDecoration(
+        color: highlight ? const Color(0xFFFFEEDB) : const Color(0xFFE6F0FA),
+        borderRadius: BorderRadius.circular(8),
+      ),
+      child: Text(
+        text,
+        style: TextStyle(
+          fontSize: 10,
+          fontWeight: FontWeight.w400,
+          color: highlight ? const Color(0xFFB86F0D) : const Color(0xFF2A6FB0),
+        ),
+      ),
+    );
+  }
+
+  String getDDay(String dateString) {
+    try {
+      final regex = RegExp(r'(\d{4})년\s*(\d{1,2})월\s*(\d{1,2})일');
+      final match = regex.firstMatch(dateString);
+      if (match != null) {
+        final year = int.parse(match.group(1)!);
+        final month = int.parse(match.group(2)!);
+        final day = int.parse(match.group(3)!);
+        final deadline = DateTime(year, month, day);
+        final now = DateTime.now();
+        final diff = deadline.difference(now).inDays;
+
+        if (diff > 0) return 'D-$diff';
+        if (diff == 0) return 'D-Day';
+        return '마감';
+      }
+      return '상시모집';
+    } catch (e) {
+      return '날짜 오류';
+    }
+  }
+}
--- pc/lib/screens/Recommands_screens.dart
+++ pc/lib/screens/Recommands_screens.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:pc/screens/Package_screens.dart';
 import 'package:pc/screens/Nav_Screens.dart';
 
 class RecommandsScreens extends StatefulWidget {
@@ -22,12 +23,15 @@ class _RecommandsScreensState extends State<RecommandsScreens> {
         title: const Row(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Text('맞춤형 추천', style: TextStyle(
-              fontFamily: "Roboto",
-              fontWeight: FontWeight.w400,
-              fontSize: 20,
-              color: Color(0xFF7B7B7B),
-            )),
+            Text(
+              '맞춤형 추천',
+              style: TextStyle(
+                fontFamily: "Roboto",
+                fontWeight: FontWeight.w400,
+                fontSize: 20,
+                color: Color(0xFF7B7B7B),
+              ),
+            ),
           ],
         ),
       ),
@@ -59,12 +63,18 @@ class _RecommandsScreensState extends State<RecommandsScreens> {
             const SizedBox(height: 30),
 
             // 패키지 카드 1
-            buildPackageSection(title: '진로 / 취업에 도움되는 패키지'),
+            buildPackageSection(
+              title: '진로 / 취업에 도움되는 패키지',
+              userId: widget.userId,
+            ),
 
             const SizedBox(height: 30),
 
             // 패키지 카드 2
-            buildPackageSection(title: '시간효율높은 포인트 집중 패키지'),
+            buildPackageSection(
+              title: '시간효율 높은 포인트 집중 패키지',
+              userId: widget.userId,
+            ),
           ],
         ),
       ),
@@ -75,12 +85,18 @@ class _RecommandsScreensState extends State<RecommandsScreens> {
           if (index == 0) {
             Navigator.pushReplacement(
               context,
-              MaterialPageRoute(builder: (_) => NavScreens(userId: widget.userId)),
+              MaterialPageRoute(
+                builder: (_) => NavScreens(userId: widget.userId),
+              ),
             );
           } else if (index == 1) {
             Navigator.pushReplacement(
               context,
-              MaterialPageRoute(builder: (_) => NavScreens(userId: widget.userId, initialIndex: index,)),
+              MaterialPageRoute(
+                builder:
+                    (_) =>
+                        NavScreens(userId: widget.userId, initialIndex: index),
+              ),
             );
           }
         },
@@ -88,84 +104,99 @@ class _RecommandsScreensState extends State<RecommandsScreens> {
         showUnselectedLabels: false,
         selectedItemColor: Color(0xFF7B7B7B),
         unselectedItemColor: Color(0xFF7B7B7B),
-        items: [BottomNavigationBarItem(icon: Icon(Icons.home_outlined),
-        label: '홈',
-        ),
-        BottomNavigationBarItem(icon: Icon(Icons.person_outline),
-        label: '마이페이지')
-        ],  
+        items: [
+          BottomNavigationBarItem(icon: Icon(Icons.home_outlined), label: '홈'),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person_outline),
+            label: '마이페이지',
+          ),
+        ],
       ),
     );
   }
 
-  Widget buildPackageSection({required String title}) {
+  Widget buildPackageSection({required String title, required String userId}) {
     final List<String> tags = ['세일즈', '디자인', '개발'];
 
-    return Container(
-      padding: const EdgeInsets.all(16),
-      decoration: BoxDecoration(
-        color: Colors.white,
-        borderRadius: BorderRadius.circular(12),
-      ),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Text(
-            title,
-            style: const TextStyle(
-              fontFamily: "Roboto",
-              fontWeight: FontWeight.w600,
-              fontSize: 18,
-              color: Color(0xFF262626),
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (_) => PackageScreens(title: title, userId: userId),
+          ),
+        );
+      },
+      child: Container(
+        padding: const EdgeInsets.all(16),
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(12),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              title,
+              style: const TextStyle(
+                fontFamily: "Roboto",
+                fontWeight: FontWeight.w600,
+                fontSize: 18,
+                color: Color(0xFF262626),
+              ),
             ),
-          ),
-          const SizedBox(height: 16),
+            const SizedBox(height: 16),
 
-          // 가로 스크롤 이미지 리스트
-          SizedBox(
-            height: 100,
-            child: ListView.builder(
-              scrollDirection: Axis.horizontal,
-              itemCount: 5,
-              itemBuilder: (context, index) {
-                return Container(
-                  width: 100,
-                  margin: const EdgeInsets.only(right: 12),
-                  decoration: BoxDecoration(
-                    color: Colors.grey[300],
-                    borderRadius: BorderRadius.circular(10),
-                  ),
-                  child: const Center(child: Text('이미지')),
-                );
-              },
+            // 가로 스크롤 이미지 리스트
+            SizedBox(
+              height: 100,
+              child: ListView.builder(
+                scrollDirection: Axis.horizontal,
+                itemCount: 5,
+                itemBuilder: (context, index) {
+                  return Container(
+                    width: 100,
+                    margin: const EdgeInsets.only(right: 12),
+                    decoration: BoxDecoration(
+                      color: Colors.grey[300],
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    child: const Center(child: Text('이미지')),
+                  );
+                },
+              ),
             ),
-          ),
 
-          const SizedBox(height: 16),
+            const SizedBox(height: 16),
 
-          // 태그
-          Wrap(
-            spacing: 8,
-            children: tags.map((tag) {
-              return Container(
-                padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 6),
-                decoration: BoxDecoration(
-                  color: const Color(0xFFFFEEDB),
-                  borderRadius: BorderRadius.circular(20),
-                ),
-                child: Text(
-                  tag,
-                  style: const TextStyle(
-                    fontSize: 12,
-                    fontWeight: FontWeight.w400,
-                    color: Color(0xFFEA7500),
-                    fontFamily: "Roboto",
-                  ),
-                ),
-              );
-            }).toList(),
-          ),
-        ],
+            // 태그
+            Wrap(
+              spacing: 8,
+              children:
+                  tags.map((tag) {
+                    return Container(
+                      padding: const EdgeInsets.symmetric(
+                        horizontal: 12,
+                        vertical: 6,
+                      ),
+                      decoration: BoxDecoration(
+                        color: const Color(0xFFFFEEDB),
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                      child: Text(
+                        tag,
+                        style: const TextStyle(
+                          fontSize: 12,
+                          fontWeight: FontWeight.w400,
+                          color: Color(0xFFEA7500),
+                          fontFamily: "Roboto",
+                        ),
+                      ),
+                    );
+                  }).toList(),
+            ),
+          ],
+        ),
       ),
     );
   }
